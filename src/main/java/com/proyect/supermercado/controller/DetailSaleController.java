6f13fe66b7d5d77bd9cd25b2f8470e2f5628ee68
paulo <paulorodriguez733@gmail.com>
2026-04-01 10:06:01 -0500

    feat: add the endpoint post

---
+++ src/main/java/com/proyect/supermercado/controller/DetailSaleController.java
@@ -0,0 +1,28 @@
+package com.proyect.supermercado.controller;
+
+import com.proyect.supermercado.dto.DetailSaleRequestDTO;
+import com.proyect.supermercado.dto.DetailSaleResponseDTO;
+import com.proyect.supermercado.service.DetailSaleService;
+
+import lombok.RequiredArgsConstructor;
+import java.util.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/details")
+public class DetailSaleController {
+
+    private final DetailSaleService detailSaleService;
+
+    @PostMapping
+    public ResponseEntity <DetailSaleResponseDTO> create(@RequestBody DetailSaleRequestDTO request){
+        DetailSaleResponseDTO response = detailSaleService.create(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+
+
+}
