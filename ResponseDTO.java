77af76bd3047f51956a6a25f5067d080bc88f63a
J3RSS0N17 <jersson1108@gmail.com>
2026-04-05 14:48:16 -0500

    Update ResponseDTO.java

---
+++ ResponseDTO.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+
+public class EmpleadoResponseDTO {private Long id;
+                                  private String cedula;
+                                  private String nombre;
+                                  private Cargo cargo;
+                                  private LocalDate fecha;
+                                  private LocalDate fechaIngreso;
+                                  private Double salario;}
